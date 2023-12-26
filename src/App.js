d80ef1e194ecfc8279d578296cd690f86d097f2a
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-26 21:09:53 +0300

    useReducer structure set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import Header from './Header'
-import Main from './Main'
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 export default function App() {
-  return(
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
     <div className="app">
       <Header />
       <Main>
@@ -10,5 +41,5 @@ export default function App() {
         <p>Question</p>
       </Main>
     </div>
-  )
+  );
 }
