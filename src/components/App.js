23c802955e593ab1b305bb8b7fa949d40f368030
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-28 23:27:16 +0300

    displaying progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from "./Error"
 import StartScreen from "./StartScreen";
 import Question from "./Question"
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -54,9 +55,10 @@ function reducer(state, action) {
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
 
-  const numQuestions = questions.length
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, currQs) => prev + currQs.points, 0)
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -73,6 +75,13 @@ export default function App() {
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
         {status === 'active' && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               answer={answer}
